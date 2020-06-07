3e9e975197408d122c7f4bb5977696a108343645
fatwaer <fatwaer@gmail.com>
2020-06-07 22:31:05 +0800

    Update: repair install.sh, it's ok now

--- install.sh
+++ install.sh
@@ -1,26 +1,29 @@
 #! /bin/bash
 
+set -o errexit
+
 DEBUG=1
 
-DotfilePATH=~/.dotfile
-# vimrc and bashrc
+DotfilePath=~/.dotfile
+# vimrc
 VimCachePath=~/.cache/vim
 VimPlugPath=~/.vim/autoload
 
 DIRTOCREATE=($DotfilePath $VimCachePath $VimPlugPath)
 
-# rc files
-rf_files=(~/.vim ~/.bashrc)
+# settings files
+target_path=(~/.vim ~/.bashrc)
+origin_path=(.vim bashrc)
 
 function dprint {
   if [[ ${DEBUG} -eq 1 ]]
   then
-      echo $1
+      echo "$1"
   fi
 }
 
 function create_directory {
-    for path in ${DIRTOCREATE}
+    for path in ${DIRTOCREATE[@]}
     do
         mkdir -p ${path}
     done
@@ -28,20 +31,19 @@ function create_directory {
 
 function create_rcfile_links {
   dprint $1
-  echo "ready to create rc_file links ..."
-  for rc_file in $1
+  echo "ready to create dotfiles links ..."
+  for ((idx=0; idx<${#origin_path[@]}; idx++))
   do
-    unlink ${rc_file}
-    ln -sf ${rc_file} ~
+      rm -r ${target_path[${idx}]}
+      ln -sf ${DotfilePath}/${origin_path[${idx}]} ${target_path[${idx}]}
   done
 }
 
 function backup_origin_file {
-  dprint $1
-  backup_path=~/.backup
+  backup_path=./backup
   echo "ready to backup original files..."
   mkdir -p ${backup_path}
-  for rc_file in $1
+  for rc_file in "$@"
   do
     cp -r ${rc_file} ${backup_path}
     echo "backup "${rc_file} to ${backup_path}
@@ -49,10 +51,21 @@ function backup_origin_file {
   echo "backup done !!"
 }
 
-function exec_intall {
-  create_directory
-  backup_origin_file ${rf_files}
-  create_rcfile_links ${rf_files}
+function curl_download {
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
 }
 
+function exec_intall {
+  create_directory
+  backup_origin_file "${target_path[@]}"
+  create_rcfile_links
+  curl_download
+}
+
+###
+# execution
+###
+
+pushd ${DotfilePath}
 exec_intall
+popd ${DotfilePath}
